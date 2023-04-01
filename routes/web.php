60ef9e3db4f350f1f161fdbf1bd4be69e7d83b42
mohamedelsaed77 <mohamedelsaed987@gmail.com>
2023-04-02 01:48:57 +0200

    edit to files

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('website.home');
 });
+
+
+Route::get('/category', function () {
+    return view('website.category');
+});
+
+Route::get('/product', function () {
+    return view('website.product');
+});
